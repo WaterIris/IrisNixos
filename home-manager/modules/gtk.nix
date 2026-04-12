956c643a872daae3b578d05457f24433161e6db8
WaterIris <179727984+WaterIris@users.noreply.github.com>
2026-04-12 23:20:09 +0200

    (add) tweak gtk a little

--- home-manager/modules/gtk.nix
+++ home-manager/modules/gtk.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }:
 
 {
-  # home.pointerCursor = {
-  #   gtk.enable = true;
-  #   package = pkgs.bibata-cursors;
-  #   name = "Bibata-Modern-Ice";
-  #   size = 20;
-  # };
   gtk = {
     enable = true;
 
